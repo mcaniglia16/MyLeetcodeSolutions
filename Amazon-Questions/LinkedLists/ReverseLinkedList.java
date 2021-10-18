8057dbe6ebf9beb93ee932201f5161ad886ac851
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-10-18 14:15:09 -0400

    Update ReverseLinkedList.java

--- Amazon-Questions/LinkedLists/ReverseLinkedList.java
+++ Amazon-Questions/LinkedLists/ReverseLinkedList.java
@@ -9,23 +9,42 @@
  * }
  */
 
- // head
- // [1] -> [2] -> [3] -> null
- // null <- [1] <- [2] <- [3] 
+ //      head
+ // x    [1] -> [2] -> [3] -> null
+
+ // prev  curr   next
+ // x     [1] -> [2] -> [3] -> null
+
+ //              curr
+ // next         prev
+ // x  <- [1]    [2] -> [3] -> null
+
+ // null <- [1] <- [2] <- [3]  x
  //                      head
 
  // [3] -> [2] -> [1] -> null
 class Solution {
     public ListNode reverseList(ListNode head) {
+
+        // Method 1) iterative, O(N) time , O(1) space
         ListNode curr = head;
         ListNode prev = null;
-        
+        ListNode next = null;
         while(curr != null){
-            ListNode temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
+            next = curr.next;   // save the next
+            curr.next = prev;   // reverse next and previous
+            prev = curr;        //
+            curr = next;        // advance previous and current
         }
         return prev;
+
+        //Method 2) recursive, O(N) time, O(N) space (stack)
+        // if (head == null || head.next == null) {
+        //     return head;
+        // }
+        // ListNode reversedHead = reverseList(head.next);
+        // head.next.next = head;
+        // head.next = null;
+        // return reversedHead;
     }
 }
