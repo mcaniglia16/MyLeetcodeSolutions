eaade3e593d9688fae88d1376c1b9ca913c20727
mcaniglia16 <50206147+mcaniglia16@users.noreply.github.com>
2021-10-21 18:03:19 -0400

    Create WordBreak.java

---
+++ Amazon-Questions/Dynamic Programming/WordBreak.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        //Solution 1: Dynamic Programming
+        // O(n^3) time
+        // O(n) space
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
